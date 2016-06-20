86eca7c30365c0ef6d8c97e99840de0a6bb3e350
Amirhosein Zolfaghari <a.zolfaghari@hamrah7.com>
2016-06-20 15:35:42 +0430

    added forgotten namespace

--- ReadableApiErrorResponseBundle/EventListener/InternalServerExceptionListener.php
+++ ReadableApiErrorResponseBundle/EventListener/InternalServerExceptionListener.php
@@ -6,6 +6,7 @@ namespace Zolfa\ReadableApiErrorResponseBundle\EventListener;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class InternalServerExceptionListener
 {
